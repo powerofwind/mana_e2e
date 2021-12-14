2ef94fbd4ab11529ddbe10cd4cf519afc1c75051
powerofwind <powerofwind10@gmail.com>
2021-12-14 10:46:17 +0700

    setup proj playwright and xunit

---
+++ manaTest/SetUpProject.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+namespace manaTest
+{
+    public class SetUpProject
+    {
+        private static IPage page;
+        private static IBrowser browser;
+        private static IPlaywright playwright;
+
+        public async Task<IBrowser> BeforeScenario()
+        {
+            playwright ??= await Playwright.CreateAsync();
+
+            if (null == browser)
+            {
+                browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                {
+                    Headless = false,
+                    SlowMo = 1000,
+                });
+                page = await browser.NewPageAsync();
+            }
+            return browser;
+        }
+
+        public async Task<string> ManaMcontent()
+        {
+            var browser = await BeforeScenario();
+            await page.GotoAsync("http://localhost:8100/#/user-profile-address");
+            await page.ClickAsync("text=กทม123 ทวีวัฒนา ทวีวัฒนา กรุงเทพมหานคร 101700632130913 >> p");
+            var ttt = string.Empty;
+            page.Dialog += async (_, dialog) =>
+            {
+                Console.WriteLine(dialog.Message);
+                ttt = dialog.Message;
+                await dialog.DismissAsync();
+            };
+            await page.WaitForTimeoutAsync(5000);
+            return ttt;
+        }
+    }
+}
---
+++ mana_XUnit/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using manaTest;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace mana_XUnit
+{
+    public class UnitTest1
+    {
+        [Fact(DisplayName = "เข้าหน้า Ticket ได้")]
+        public async Task Test1()
+        {
+            var sut = new SetUpProject();
+            sut.ManaMcontent();
+
+            var res = await sut.ManaMcontent();
+            res.Should().Be("");
+        }
+    }
+}
